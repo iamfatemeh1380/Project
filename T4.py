d27aecbe991ce9e8e0461e7ac6f5693348e316df
iamfatemeh1380 <zangenehfatemeh1683@gmail.com>
2022-12-11 08:46:11 -0800

    this is t4

---
+++ T4.py
@@ -0,0 +1,12 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n=4
+>>> for i in range(1,n+1):
+	print((i)*' '+(-2*i+(2*n-1))*'*')
+
+	
+ *****
+  ***
+   *
+    
+>>> 
